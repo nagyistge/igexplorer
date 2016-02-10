07160423b53aff88132af1b9863456f56b068e0d
Bill Donner <bdonner@inmarkit.com>
2016-02-10 08:43:34 -0500

    First Commit

--- igexplorer/ViewController.swift
+++ igexplorer/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("First commit new app")
     }
 
     override func didReceiveMemoryWarning() {
